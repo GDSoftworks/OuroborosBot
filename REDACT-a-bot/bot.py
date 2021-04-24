2cb8c7b75027fa7bf22c408eb6c5365cb779cc1b
David D <daidavid0609@gmail.com>
2021-04-24 16:57:46 +0200

    New folder

---
+++ REDACT-a-bot/bot.py
@@ -0,0 +1,31 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN') #edit .env file
+GUILD = os.getenv("DISCORD_GUILD")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print( #prints if the bot is connected to the server
+        f'{client.user} is connected to the following server:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+#New Member join
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+
+client.run(TOKEN)
