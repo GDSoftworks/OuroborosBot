9880debc323ffdef0d37bca062001f770c894daf
david-0609 <daidavid0609@gmail.com>
2021-06-13 13:41:44 +0200

    Added kick user function

--- REDACT-a-bot/bot.py
+++ REDACT-a-bot/bot.py
@@ -1,5 +1,6 @@
 import discord
 from deep_translator import GoogleTranslator
+from time import sleep
 
 TOKEN = (open("TOKEN", "r")).readline()
 GUILD = (open("GUILD", "r")).readline()
@@ -17,18 +18,17 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+#Reads in list of swears
 cleaned_list = []
 with open("list.txt", "r") as swear_list:
     swear_list=swear_list.readlines()
     for swear in range(len(swear_list)):
-        #swear = swear.replace("\n", "")
         swear = swear_list[swear].replace("\n", '')
         cleaned_list.append(swear)
 
-#print(cleaned_list)
-# user = ""
 recorded_swears = 0
-# counter = {user: recorded_swears}
+warning_count = 9
+kick_count = 10
 
 #Detects and removes swears
 @client.event
@@ -38,7 +38,6 @@ async def on_message(message):
     author = message.author
     translated = GoogleTranslator(source='auto', target='en').translate(msg)
     translated_msg = translated.lower()
-    #print(translated)
     for word in cleaned_list:
         if word in translated_msg:
             recorded_swears = recorded_swears + 1
@@ -46,9 +45,17 @@ async def on_message(message):
             await message.delete()
             await message.channel.send(message.author.mention+" Dont use that word 🙊! This is a warning")
             await log_output(author, recorded_swears)
-            
-log_channel_id = 852202389896560650
+            if recorded_swears >= warning_count:
+                await message.channel.send(message.author.mention+" This is your last warning, you will be kicked")
+                if recorded_swears >= kick_count:
+                    await message.channel.send(message.author.mention+" You will be kicked in 10 seconds")
+                    sleep(10)
+                    print("Kicked")
+                    await log_kick(author, recorded_swears)
 
+           
+log_channel_id = 852202389896560650
+#send message ti logging channel
 async def log_output(author, recorded_swears):
     channel = client.get_channel(log_channel_id)
     author = str(author)
@@ -56,4 +63,13 @@ async def log_output(author, recorded_swears):
     logmsg="Number of swears recorded for: {0} = {1}"
     await channel.send(logmsg.format(author, recorded_swears))
 
+async def log_kick(author, recorded_swears):
+    channel = client.get_channel(log_channel_id)
+    author = str(author)
+    recorded_swears = str(recorded_swears)
+    logmsg="Kicked {0}, {0} has used swears for {1} times"
+    await channel.send(logmsg.format(author, recorded_swears))
+    #Uncomment to enable kick
+    #await discord.Guild.kick(author)
+    
 client.run(TOKEN)
